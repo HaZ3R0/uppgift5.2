bf4f0296ca91c7c9cee98ecb04d4ccca22db25ea
HaZ3R0 <megamackan@outlook.com>
2023-11-22 10:08:16 +0100

    Add project files.

---
+++ uppgift5.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace uppgift5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            string taltext = Console.ReadLine();
+            int talet = int.Parse(taltext);
+            int[] tal = { 1, 2, talet };
+            Console.WriteLine(tal[0] + " "  + tal[1] + " " + tal[2]);
+        }
+    }
+}
